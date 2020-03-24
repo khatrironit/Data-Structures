6ef9604a26ea65c1771871f5d957e1df124d1e4f
khatrironit <ronitkhatri1999@gmail.com>
2020-03-25 01:02:05 +0530

    gcd

---
+++ 12.Number Theory/4.Advanced GCD.cpp
@@ -0,0 +1,28 @@
+// find gcd(a,b) where a<10^250(can't be stored in int or long long) and b<40,000
+
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int advanced_gcd(string a,int b){
+	int prev_gcd = int(a[0]-'0')%b;
+	int curr_gcd = prev_gcd;
+
+	int i=1;
+	while(a[i]!='\0'){
+		curr_gcd = ((prev_gcd*10)%b + int(a[i]-'0')%b )%b;
+		prev_gcd = curr_gcd;
+		i++;
+	}
+	return __gcd(b,curr_gcd);
+}
+int main(){
+	string a;
+	cin>>a;
+	int b;
+	cin>>b;
+	cout<<advanced_gcd(a,b);
+	return 0;
+
+}
