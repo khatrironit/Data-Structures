770bed119ca2cedb43ac3f1e240a4abbbe536d38
khatrironit <ronitkhatri1999@gmail.com>
2020-03-25 01:01:42 +0530

    extended euclid algo

---
+++ 12.Number Theory/3.Multiplicative modulo inverse.cpp
@@ -0,0 +1,24 @@
+//if (A.B)%m = 1 then B is the multiplicative modulo inverse of A. A and m must be co-prime.
+
+
+#include<iostream>
+using namespace std;
+
+pair<int,int>modulo_inverse(int A,int m){
+	if(m==0){
+		pair<int,int> p(1,0);
+		return p;
+	}
+	pair<int,int> small_ans = modulo_inverse(m,A%m);
+	pair<int,int> ans;
+	ans.first = small_ans.second;
+	ans.second = small_ans.first - (A/m)*small_ans.second;
+	return ans;
+}
+int main(){
+	int A = 5;
+	int m = 17;
+	pair<int,int>ans = modulo_inverse(A,m);
+	cout<<ans.first;
+	return 0;
+}
