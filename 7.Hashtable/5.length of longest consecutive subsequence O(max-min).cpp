8ce205482bc4b124bcfb6811d6ee7350481a7ee9
khatrironit <ronitkhatri1999@gmail.com>
2020-03-27 01:17:32 +0530

    hashing

---
+++ 7.Hashtable/5.length of longest consecutive subsequence O(max-min).cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	vector<int> v(n);
+	for(int i=0;i<n;i++){
+		cin>>v[i];
+	}
+
+	int max_ = *max_element(v.begin(),v.end());
+	int min_ = *min_element(v.begin(),v.end());
+	map<int,int>m;
+	for(int i=0;i<n;i++){
+		m[v[i]]++;
+	}
+	int count = 0;
+	int ans = 0;
+	for(int i=min_;i<=max_;i++){
+		if(m.find(i+1)!=m.end()){
+			count++;
+		}else{
+			ans = max(ans,count+1);
+			count = 0;
+		}
+	}
+	cout<<ans;
+	return 0;
+}
