e5e3dd8c05ac0edc1fd5e0592b240d42140df8bd
khatrironit <ronitkhatri1999@gmail.com>
2020-03-26 23:52:47 +0530

    hashing

---
+++ 7.Hashtable/2.check subarray with sum zero.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<set>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int * arr = new int[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	bool present = false;
+	set<int>s;
+	int prefix = 0;
+	for(int i=0;i<n;i++){
+		prefix += arr[i];
+
+		if(prefix == 0 || s.find(prefix) != s.end()){
+			present = true;
+			break;
+		}
+		s.insert(prefix);
+	}
+	if(present)
+		cout<<"zero sum subarray is present";
+	else
+		cout<<"zero sum subarray is not present";
+	delete [] arr;
+	return 0;
+}
